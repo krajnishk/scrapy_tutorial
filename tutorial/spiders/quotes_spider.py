28968efa440e982307bac1706bb759fc6d1be148
rajnish <krajnishk@protonmail.com>
2021-03-18 16:50:38 +0530

    extracting data in a file including pagination

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -12,9 +12,15 @@ class QuoteSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = os.path.join(
-            "C:\\myprojects\\sc_tuts\\tutorial\\tmp", f'quotes-{page}.html')
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log("scraping is done")
+        """ Parse the html page to extract the data """
+        for quote in response.xpath("//div[@class='quote']"):
+            yield {
+                'text': quote.xpath(".//span[@class='text']/text()").get(),
+                'author': quote.xpath(".//small[@class='author']/text()").get(),
+                'tags': quote.xpath(".//div[@class = 'tags']/a[@class='tag']/text()").getall()
+            }
+
+        #   Pagination, handling next pages
+        next_page_url = response.xpath('//li[@class="next"]/a/@href').get()
+        if next_page_url is not None:
+            yield response.follow(next_page_url, callback=self.parse)
